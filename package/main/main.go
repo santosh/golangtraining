aa682f98e6fdd9744bd676a421de7944af7e370f
Santosh Kumar <sntshkmr60@gmail.com>
2018-10-28 20:20:09 +0530

    package structure representation

---
+++ package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "gitlab.com/sntshk/golangtraining/package/stringutil"
+)
+
+func main() {
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
+    fmt.Println(stringutil.MyName)
+}
---
+++ package/stringutil/reverse.go
@@ -0,0 +1,7 @@
+// package stringutil contains utility functions for working with reverse strings
+package stringutil
+
+// Reverse returns its argument string reversed rune-wise left to right
+func Reverse(s string) string {
+    return reverseTwo(s)
+}
