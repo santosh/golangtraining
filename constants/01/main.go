2cd20c3ad0705edc1e8090dcbf29705d529cbd27
Santosh Kumar <sntshkmr60@gmail.com>
2018-10-31 18:07:14 +0530

    constants

---
+++ constants/01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+const p string = "death & taxes"
+
+func main() {
+    const q = 42
+    const s = "string"
+    fmt.Println("p -", p)
+    fmt.Println("q -", q)
+    fmt.Println("s -", s)
+}
