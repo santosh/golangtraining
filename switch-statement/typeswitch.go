4eaeeb9bdbb2c31aa016ad0339fdb0ec36ce3e72
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-01 06:34:24 +0530

    switching on datatypes

---
+++ switch-statement/typeswitch.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Contact struct {
+    greeting    string
+    name        string
+}
+
+func SwitchOnType(x interface{}) {
+    switch x.(type) {
+    case int:
+        fmt.Println("int")
+    case string:
+        fmt.Println("string")
+    case Contact:
+        fmt.Println("Contact")
+    default:
+        fmt.Println("unknown")
+    }
+}
+
+func main() {
+    SwitchOnType(7)
+    SwitchOnType("McLeod")
+    var t = Contact{"Good to see you,", "Tim"}
+    SwitchOnType(t)
+    SwitchOnType(t.greeting)
+    SwitchOnType(t.name)
+}
