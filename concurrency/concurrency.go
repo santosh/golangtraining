69c2e8f01942aeef4ac73dfef796a7d251553ca9
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-01 09:30:13 +0530

    syncing goroutines

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -3,16 +3,23 @@ package main
 import (
     "fmt"
     "time"
+    "sync"
 )
 
-func say(s string ) {
+var wg sync.WaitGroup
+
+func say(s string) {
     for i := 0; i < 3; i++ {
         fmt.Println(s)
         time.Sleep(time.Millisecond * 100)
     }
+    wg.Done()
 }
 
 func main() {
+    wg.Add(1)
     go say("Hey")
-    say("There")
+    wg.Add(1)
+    go say("There")
+    wg.Wait()
 }
