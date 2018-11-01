aab50ad1ca57c136708c3ec72bfaae6669ac5bdb
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-01 06:22:52 +0530

    break is there by defualt, use fallthrough to fall
    use fallthrough to fall down

---
+++ switch-statement/fallthrough.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+)
+
+/*
+ *func main() {
+ *    switch "Marcus" {
+ *    case "Tim":
+ *        fmt.Println("Wassap Tim")
+ *    case "Marcus":
+ *        fmt.Println("Wassap Marcus")
+ *        fallthrough
+ *    case "Medhi":
+ *        fmt.Println("Wassap Medhi")
+ *        fallthrough
+ *    case "Julian":
+ *        fmt.Println("Wassap Julian")
+ *    case "Jenny":
+ *        fmt.Println("Wassap Jenny")
+ *    case "Sushant":
+ *        fmt.Println("Wassap Sushant")
+ *    }
+ *}
+ */
+
+func main() {
+    
+}
