ef234bfc6674ebf9e422f2237f3080f3fa091c30
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-01 08:09:21 +0530

    multiple returns

--- functions/returns.go
+++ functions/returns.go
@@ -8,7 +8,6 @@ func main() {
     fmt.Println(greet("Jane ", "Doe"))
 }
 
-func greet(fname, lname string) (s string) {
-    s = fmt.Sprint(fname, lname)
-    return
+func greet(fname, lname string) (string, string) {
+    return fmt.Sprint(fname, lname), fmt.Sprint(lname, fname)
 }
