5a6941f0a665dc43ec831887ec1f16da10734b3d
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-01 11:27:40 +0530

    variadic args with slight difference

--- functions/variadic-args.go
+++ functions/variadic-args.go
@@ -6,11 +6,11 @@ import (
 
 func main() {
     data := []float64{43, 56, 87, 12, 45, 57}
-    n := average(data...)  // this is how we flatten slices
+    n := average(data)  // this is how we flatten slices
     fmt.Println(n)
 }
 
-func average(sf ...float64) float64 {  // ... is like *sf in Python
+func average(sf []float64) float64 {  // ... is like *sf in Python
     fmt.Println(sf)
     fmt.Printf("%T\n", sf)
     total := 0.0
