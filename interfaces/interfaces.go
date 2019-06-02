7a88ee3d823257f40bee4d359dd7fe42fe4b1471
Santosh Kumar <sntshkmr60@gmail.com>
2019-06-02 17:06:28 +0530

    Intro to interfaces

---
+++ interfaces/interfaces.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type Square struct {
+	side float64
+}
+
+func (z Square) area() float64 {
+	return z.side * z.side
+}
+
+type Shape interface {
+	area() float64
+}
+
+func info(z Shape) {
+	fmt.Println(z)
+	log.Println(z.area())
+}
+
+func main() {
+	s := Square{10}
+	info(s)
+}
