a06c1bf7425d34e78bb53ccf31ad430ef7801107
Santosh Kumar <sntshkmr60@gmail.com>
2019-06-02 17:07:18 +0530

    Explanation to interfaces
    Interfaces are like middleware, see it as adapter pattern.
    You implement an interfaces. Methods with same signature can
    be adapted to it.

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -3,10 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 )
 
 type Square struct {
 	side float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
 }
 
 func (z Square) area() float64 {
@@ -24,5 +33,7 @@ func info(z Shape) {
 
 func main() {
 	s := Square{10}
+	c := Circle{10}
 	info(s)
+	info(c)
 }
