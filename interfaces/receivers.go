ec0b24c0496058473f707bd376be89e50a6b5873
Santosh Kumar <sntshkmr60@gmail.com>
2019-06-03 08:04:46 +0530

    pointer receivers

--- interfaces/receivers.go
+++ interfaces/receivers.go
@@ -5,8 +5,6 @@ import (
 	"math"
 )
 
-// value receivers
-
 type circle struct {
 	radius float64
 }
@@ -15,6 +13,10 @@ type shape interface {
 	area() float64
 }
 
+// if the reiever is value receiver,
+// we can pass it either value or pointer
+// But if reveiver is pointer,
+// we can only pass pointer
 func (c circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
@@ -25,5 +27,6 @@ func info(s shape) {
 
 func main() {
 	c := circle{5}
-	info(c)
+	info(&c)
+	// info(c) // wrong
 }
