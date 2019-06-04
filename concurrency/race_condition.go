9a5e595f46f027db41657e5edcb241f29adf282c
Santosh Kumar <sntshkmr60@gmail.com>
2019-06-04 10:35:33 +0530

    Race condition solution - Mutex

--- concurrency/race_condition.go
+++ concurrency/race_condition.go
@@ -11,15 +11,16 @@ import (
 // memory (variable)
 
 var wg sync.WaitGroup
+var mutex sync.Mutex
 var counter int
 
 func incrementor(s string) {
 	for i := 0; i < 20; i++ {
-		x := counter
-		x++
+		mutex.Lock()
+		counter++
 		time.Sleep(time.Duration(rand.Intn(3)) * time.Millisecond)
-		counter = x
 		fmt.Println(s, i, "Counter: ", counter)
+		mutex.Unlock()
 	}
 	wg.Done()
 }
