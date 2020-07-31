741530925b68fa56c7355683e7d919cf1724d2cd
Santosh Kumar <sntshkmr60@gmail.com>
2020-07-31 11:51:08 +0530

    Add two goroutines

--- concurrency/yt_e4bu9g-bYtg.go
+++ concurrency/yt_e4bu9g-bYtg.go
@@ -1,20 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // SendValues takes a channel and sends arbitrary data to it.
-func SendValues(c chan int) {
-	c <- 8
+func SendValues(c chan string) {
+	fmt.Println("Executing Goroutine")
+	time.Sleep(1 * time.Second)
+	c <- "Hello World"
+	fmt.Println("Finished Executing Goroutine")
 }
 
 func main() {
 	fmt.Println("Go Channels Tutorial")
 
-	values := make(chan int)
+	values := make(chan string, 2)
 	defer close(values)
 
+	go SendValues(values)
 	go SendValues(values)
 
 	value := <-values
 	fmt.Println(value)
+
+	time.Sleep(1 * time.Second)
 }
